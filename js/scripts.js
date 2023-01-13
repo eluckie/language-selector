96dff689c195fcd7427f40e391282db848dcf240
E Luckie <eluckie.d@gmail.com>
2023-01-13 12:09:32 -0700

    add error message if user fails to input their name.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,35 @@
-function hideResults() {
+function hideResultsAndError() {
   document.getElementById("ruby").setAttribute("class", "hidden");
   document.getElementById("csharp").setAttribute("class", "hidden");
   document.getElementById("javascript").setAttribute("class", "hidden");
   document.getElementById("python").setAttribute("class", "hidden");
   document.getElementById("none").setAttribute("class", "hidden");
+  document.getElementById("error-message").setAttribute("class", "hidden");
 }
 
 window.addEventListener("load", function() {
   let form = document.getElementById("userForm");
   form.onsubmit = function(event) {
     event.preventDefault();
-    hideResults();
+    hideResultsAndError();
 
     const animal = parseInt(document.getElementById("animals").value);
     const fam = parseInt(document.getElementById("family").value);
+    const name = document.getElementById("user-name").value;
+    const bday = document.getElementById("birthday");
 
-      if (animal <= 2 && fam <= 2) {
+      if (name && animal <= 2 && fam <= 2) {
         document.getElementById("javascript").removeAttribute("class");
-      } else if (animal === 5 && fam === 5) {
+      } else if (name && animal === 5 && fam === 5) {
         document.getElementById("none").removeAttribute("class");
-      } else if (animal === 3 || fam === 3) {
+      } else if (name && animal === 3 || fam === 3) {
         document.getElementById("python").removeAttribute("class");
-      } else if (animal === 4 || family === 4) {
+      } else if (name && animal === 4 || family === 4) {
         document.getElementById("csharp").removeAttribute("class");
-      } else {
+      } else if (name) {
         document.getElementById("ruby").removeAttribute("class");
+      } else {
+        document.getElementById("error-message").removeAttribute("class");
+      }
     };
-  };
-})
-
-  
-// animals 1-2 && family 1-2- java
-// animals 5 && family 5 - none
-// animals 3 || family 3 - python
-// animals 4 || family 4 - c#
-// (else) - ruby
+  })
